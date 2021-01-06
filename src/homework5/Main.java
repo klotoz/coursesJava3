7d2bf622cd500a35a8bb3ac695bc7f7632009b88
klotoz <ikskulfg@gmail.com>
2021-01-06 16:16:52 +0300

    3 modified

--- src/homework5/Main.java
+++ src/homework5/Main.java
@@ -2,23 +2,34 @@ package homework5;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 
 public class Main {
-        public static final int CARS_COUNT = 4;
+    private static final int CARS_COUNT = 4;
+    static final int HALF_CARS_COUNT = CARS_COUNT / 2;
 
 
-        public static void main(String[] args) {
+        public static void main (String[]args){
             System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+            CyclicBarrier cyclicBarrier = new CyclicBarrier(CARS_COUNT+1);
+            CountDownLatch countDownLatch = new CountDownLatch(CARS_COUNT);
             Race race = new Race(new Road(60), new Tunnel(), new Road(40));
             Car[] cars = new Car[CARS_COUNT];
             for (int i = 0; i < cars.length; i++) {
-                cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+                cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cyclicBarrier, countDownLatch);
             }
-            for (int i = 0; i < cars.length; i++) {
-                new Thread(cars[i]).start();
+            for (Car car : cars) {
+                new Thread(car).start();
             }
-            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            try {
+                cyclicBarrier.await();
+                System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+                cyclicBarrier.await();
+                cyclicBarrier.await();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
         }
     }
-
